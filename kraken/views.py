b9a93b2b68454eda0359610d6d5c87f703f84e13
sebatyler <sebatyler@gmail.com>
2022-08-16 00:44:41 +0900

    Get bitcoin price and order fake

--- kraken/views.py
+++ kraken/views.py
@@ -15,8 +15,29 @@ class IndexView(TemplateView):
         api = krakenex.API(key, secret)
         k = KrakenAPI(api)
         balance = k.get_account_balance()
-        data['balance'] = dict(sorted(balance.to_dict()['vol'].items()))
+        balance = {k: dict(amount=v) for k, v in sorted(balance.to_dict()['vol'].items())}
 
-        # k.add_standard_order(pair="ATOMXBT", type="buy", ordertype="limit", volume="420.0", price="0.00042", validate=False)
+        print(balance)
+
+        pair = 'XXBTZEUR'
+        ticker = k.get_ticker_information(pair).to_dict()
+        # average of ask, bid
+        price = (float(ticker['a'][pair][0]) + float(ticker['b'][pair][0])) / 2
+        balance['XXBT']['price'] = price
+
+        for v in balance.values():
+            if 'price' in v:
+                v['my_price'] = f"{v['price'] * v['amount']:,.2f} Euros"
+
+        data['balance'] = balance
+
+        # buy Bitcoin
+        pair = 'XXBTZEUR'
+        btc_price = balance['XXBT']['price']
+        amount = 5 / btc_price
+        print(btc_price, amount)
+
+        r = k.add_standard_order(pair=pair, type="buy", ordertype="market", volume=amount, validate=True)
+        print(r)
 
         return data
