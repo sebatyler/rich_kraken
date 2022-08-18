a5c94dbffeffde4611fd4eeb3089892c80d2f8f1
sebatyler <sebatyler@gmail.com>
2022-08-19 07:07:50 +0900

    Modify hour

--- rich_kraken/settings.py
+++ rich_kraken/settings.py
@@ -133,5 +133,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRONJOBS = [
-    ('15 8 * * *', 'kraken.cron.buy_bitcoin', '>> /tmp/buy_bitcoin.log')
+    ('15 7 * * *', 'kraken.cron.buy_bitcoin', '>> /tmp/buy_bitcoin.log')
 ]
+
