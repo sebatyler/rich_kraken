dca98ef64cf7b4d7502b5803a99925ac6d589236
sebatyler <sebatyler@gmail.com>
2022-08-22 11:36:55 +0900

    Show euro balance

--- kraken/cron.py
+++ kraken/cron.py
@@ -18,12 +18,15 @@ def buy_bitcoin():
     is_test = False
     r = kraken.api.add_standard_order(pair=pair, type="buy", ordertype="market", volume=amount, validate=is_test)
     print(r)
-    order = r['descr']['order']
 
     # current balance and value after order
     balance = kraken.get_account_balance()
     btc_amount = balance['XXBT']['amount']
     btc_value = btc_amount * btc_price
+    euro_amount = balance['ZEUR']['amount']
 
     bot = telegram.Bot(environ['TELEGRAM_BOT_TOKEN'])
-    bot.sendMessage(chat_id=environ['TELEGRAM_BOT_CHANNEL_ID'], text=f"Order: {order}\nBTC: {btc_amount} {btc_value:,.2f} Euros")
+    bot.sendMessage(
+        chat_id=environ['TELEGRAM_BOT_CHANNEL_ID'],
+        text=f"Buy: {amount:,.8f} BTC\n{btc_amount:,.5f}BTC {btc_value:,.2f}€ / {euro_amount:,.2f}€"
+    )
